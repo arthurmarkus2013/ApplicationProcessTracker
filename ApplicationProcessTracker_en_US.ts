a2f023fd9aa4ecd62f4be2a3d7cedc0ee8a6d041
Arthur Markus <arthurmarkus2013@outlook.com>
2023-02-06 13:23:45 +0100

    Updated language file and CMake file

--- ApplicationProcessTracker_en_US.ts
+++ ApplicationProcessTracker_en_US.ts
@@ -1,3 +1,128 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"></TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AddEntryDialog</name>
+    <message>
+        <location filename="addentrydialog.ui" line="14"/>
+        <source>Add Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addentrydialog.ui" line="26"/>
+        <source>Company Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addentrydialog.ui" line="39"/>
+        <source>Applied On:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addentrydialog.ui" line="52"/>
+        <source>Latest Status:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BusinessLogic</name>
+    <message>
+        <location filename="businesslogic.cpp" line="116"/>
+        <source>Applied On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="businesslogic.cpp" line="120"/>
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="businesslogic.cpp" line="147"/>
+        <source>My Applications</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Application Process Tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="24"/>
+        <source>&amp;Add Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="44"/>
+        <source>&amp;Export As PDF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="14"/>
+        <source>Company Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="14"/>
+        <source>Applied On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="14"/>
+        <source>Latest Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="14"/>
+        <source>Updated On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="53"/>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="68"/>
+        <source>Add Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="79"/>
+        <source>Export To...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="80"/>
+        <source>Portable Document Format (*.pdf);;All Files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="108"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="121"/>
+        <source>Edit Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="129"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="131"/>
+        <source>Delete Row</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="131"/>
+        <source>Do you really wanna delete this row?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
